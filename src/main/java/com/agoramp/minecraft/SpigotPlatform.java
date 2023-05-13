e55b534088740bd59e56ca81fd13c3847898ef5f
Justin Schreiber <justintschreiber@gmail.com>
2023-05-13 10:36:32 -0700

    spigot platform works, implementing UI

--- src/main/java/com/agoramp/minecraft/SpigotPlatform.java
+++ src/main/java/com/agoramp/minecraft/SpigotPlatform.java
@@ -6,18 +6,23 @@ import com.agoramp.minecraft.util.api.text.MiniMessageUtil;
 import com.agoramp.minecraft.util.api.text.Translatable;
 import com.agoramp.minecraft.util.controller.TranslationController;
 import com.agoramp.minecraft.util.data.packets.models.ModelledPacket;
-import com.agoramp.minecraft.util.data.packets.models.packets.CloseWindowPacket;
-import com.agoramp.minecraft.util.data.packets.models.packets.WindowItemsPacket;
+import com.agoramp.minecraft.util.data.packets.models.ClickWindowPacket;
+import com.agoramp.minecraft.util.data.packets.models.CloseWindowPacket;
+import com.agoramp.minecraft.util.data.packets.models.WindowItemsPacket;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.utility.MinecraftFields;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -38,6 +43,23 @@ public enum SpigotPlatform implements Platform {
     public void load() {
         manager = ProtocolLibrary.getProtocolManager();
         MinecraftUtil.initialize(this);
+        manager.addPacketListener(new PacketAdapter(AgoraSpigot.INSTANCE, PacketType.Play.Client.WINDOW_CLICK) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                ModelledPacket original = new ClickWindowPacket(
+                        packet.getIntegers().read(0),
+                        packet.getIntegers().read(1),
+                        packet.getIntegers().read(2),
+                        packet.getShorts().read(0),
+                        packet.getItemModifier().read(0),
+                        packet.getEnumModifier(ClickWindowPacket.InventoryClickType.class, 5).read(0)
+                );
+                if (handlePacket(event.getPlayer().getUniqueId(), original)) {
+                    event.setCancelled(true);
+                }
+            }
+        });
     }
 
     @Override
@@ -49,13 +71,13 @@ public enum SpigotPlatform implements Platform {
     public int openInventory(UUID uuid, int size, ComponentLike componentLike) {
         if (size % 9 != 0) throw new Error("Invalid size");
         Player player = Bukkit.getPlayer(uuid);
-        int windowId = windowCounts.compute(uuid, (k,v) -> v == null ? 65000 : v + 1);
+        int windowId = windowCounts.compute(uuid, (k,v) -> v == null ? 124 : v + 1);
         PacketContainer container = new PacketContainer(PacketType.Play.Server.OPEN_WINDOW);
         container.getIntegers()
                 .write(0, windowId)
-                .write(1, size / 9 + 1);
+                .write(1, size / 9 - 1);
         container.getChatComponents()
-                        .write(2, serialize(uuid, componentLike));
+                        .write(0, serialize(uuid, componentLike));
         manager.sendServerPacket(player, container);
         return windowId;
     }
@@ -71,12 +93,13 @@ public enum SpigotPlatform implements Platform {
             WindowItemsPacket<ItemStack> items = (WindowItemsPacket<ItemStack>) modelledPacket;
             container = new PacketContainer(PacketType.Play.Server.WINDOW_ITEMS);
             container.getIntegers()
-                    .write(0, items.getWindowId())
-                    .write(1, items.getItems().length);
+                    .write(0, items.getWindowId());
+            List<ItemStack> stacks = new ArrayList<>();
             for (int i = 0; i < items.getItems().length; i++) {
                 Object[] metadata = items.getMetadata()[i];
                 if (metadata == null) metadata = new Object[0];
                 ItemStack stack = items.getItems()[i];
+                if (stack == null) stack = new ItemStack(Material.AIR);
                 ItemMeta meta = stack.getItemMeta();
                 if (meta != null) {
                     if (metadata.length >= 1 && metadata[0] != null) {
@@ -93,8 +116,9 @@ public enum SpigotPlatform implements Platform {
                     }
                     stack.setItemMeta(meta);
                 }
+                stacks.add(stack);
             }
-            container.getItemArrayModifier().write(2, items.getItems());
+            container.getItemListModifier().write(0, stacks);
         } else {
             throw new Error("Unhandled packet type " + modelledPacket.getClass().getSimpleName());
         }
--- src/main/java/com/agoramp/minecraft/ui/ListingUI.java
+++ src/main/java/com/agoramp/minecraft/ui/ListingUI.java
@@ -1,22 +1,144 @@
 package com.agoramp.minecraft.ui;
 
 import com.agoramp.minecraft.controller.ListingController;
+import com.agoramp.minecraft.models.graphql.ListingQuery;
+import com.agoramp.minecraft.models.graphql.fragment.CartInfo;
+import com.agoramp.minecraft.models.graphql.fragment.Product;
 import com.agoramp.minecraft.util.api.text.Translation;
 import com.agoramp.minecraft.util.api.ui.UserInterface;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class ListingUI extends UserInterface<ItemStack> {
+    private int expandedCategory = -1;
+    private ListingQuery.Category current;
+    private List<ListingQuery.Category> listing;
+    private List<Product> products;
+    private CartInfo cart;
 
     public ListingUI() {
         super(54, Translation.create("ui.listing.title",
                 "shop", ListingController.INSTANCE.getShop().title,
                 "category", ListingController.INSTANCE.getListing().get(0).title
         ));
+        listing = ListingController.INSTANCE.getListing();
+        products = listing.get(0).products.stream()
+                .map(p -> p.product)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void open(UUID id, boolean render) {
+        Player player = Bukkit.getPlayer(id);
+        if (player == null || !player.isOnline()) return;
+        super.open(id, render);
+        ListingController.INSTANCE
+                .getListing(player)
+                .subscribe(t -> {
+                    listing = t.getT2();
+                    cart = t.getT1();
+                });
     }
 
     @Override
     protected void loadItems() {
-        setItem(0, new ItemStack(Material.BLACK_WOOL), Translation.create("test"));
+        // Set title
+        rename(Translation.create("ui.listing.title",
+                "shop", ListingController.INSTANCE.getShop().title,
+                "category", ListingController.INSTANCE.getListing().get(0).title
+        ), false);
+
+        // Create background
+        ItemStack backgroundItem = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+        ComponentLike backgroundName = Component.empty();
+        row(1, backgroundItem, backgroundName);
+        row(5, backgroundItem, backgroundName);
+        col(0, backgroundItem, backgroundName);
+        col(8, backgroundItem, backgroundName);
+        col(6, backgroundItem, backgroundName);
+        setItem(slot(7, 1), null);
+        setItem(slot(7, 0), backgroundItem, backgroundName);
+
+        // Category listing
+        for (int i = 0; i < Math.min(5, listing.size()); i++) {
+            ListingQuery.Category category = listing.get(i);
+            Component name = Component
+                    .text(category.title)
+                    .decorate(TextDecoration.BOLD);
+            setItem(slot(i + 1, 0), new ItemStack(Material.CHEST), name);
+            if (expandedCategory == i) {
+                for (int j = 0; j < Math.min(5, category.subcategories.size()); j++) {
+                    Component subName = Component
+                            .text(category.subcategories.get(i).title)
+                            .decorate(TextDecoration.BOLD);
+                    setItem(slot(i, j + 1), backgroundItem, backgroundName);
+                    setItem(slot(i + 2, j + 1), backgroundItem, backgroundName);
+                    setItem(slot(i + 1, j + 1), new ItemStack(Material.CHEST), subName);
+                }
+            }
+        }
+
+        // Product listing
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            Component name = Component
+                    .text(product.title)
+                    .decorate(TextDecoration.BOLD)
+                    .append(Component.text(" - "))
+                    .append(Component.text(product.price.price.toString())
+                            .color(NamedTextColor.GREEN)
+                            .append(Objects.equals(product.price.price, product.price.listPrice) ?
+                                    Component.empty() :
+                                    Component.text(product.price.listPrice.toString()).decorate(TextDecoration.STRIKETHROUGH))
+                    );
+
+        }
+    }
+
+    private void row(int row, ItemStack item, Object... meta) {
+        for (int x = 0; x < 9; x++) {
+            setItem(slot(x, row), item, meta);
+        }
+    }
+
+    private void col(int col, ItemStack item, Object... meta) {
+        for (int y = 0; y < getSize() / 9; y++) {
+            setItem(slot(col, y), item, meta);
+        }
+    }
+
+    @Override
+    protected void loadListeners() {
+        // Category selection
+        for (int i = 0; i < Math.min(5, listing.size()); i++) {
+            ListingQuery.Category category = listing.get(i);
+            if (expandedCategory == i) {
+                addSlotListener(i + 1, () -> expandedCategory = -1);
+                for (int j = 0; j < Math.min(5, category.subcategories.size()); j++) {
+                    int k = j;
+                    addSlotListener(slot(i + 1, j + 1), () -> products = category.subcategories.get(k).products.stream()
+                            .map(p -> p.product)
+                            .collect(Collectors.toList()));
+                }
+            } else if (category.subcategories.isEmpty()) {
+                addSlotListener(i + 1, () -> products = category.products.stream()
+                        .map(p -> p.product)
+                        .collect(Collectors.toList()));
+            } else {
+                int j = i;
+                addSlotListener(i + 1, () -> expandedCategory = j);
+            }
+        }
     }
 }
